55cb56585946e416eedab93a007d17f5a7965ce3
Mamadou-Sylla <62559817+Mamadou-Sylla@users.noreply.github.com>
2022-07-20 11:18:38 +0000

    Amelioration du modification de password

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Personne;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
@@ -13,18 +15,26 @@ class PersonneController extends AbstractController
 {
     private $tokenStorage;
     private $encoder;
-    public function __construct(TokenStorageInterface $tokenStorage, UserPasswordHasherInterface $encoder){
+    private $serializer;
+    public function __construct(SerializerInterface $serializer, TokenStorageInterface $tokenStorage, UserPasswordHasherInterface $encoder){
 
         $this->tokenStorage = $tokenStorage;
         $this->encoder = $encoder;
+        $this->serializer = $serializer;
     }
     
-    public function __invoke(Personne $personne): Personne
+    public function __invoke(Personne $personne, Request $request): Personne
     {
         $data = $this->tokenStorage->getToken()->getUser();
+        $user = $request->getContent();
+        $user = $this->serializer->decode($user, "json");
+       
         if($data->getId() == null || $data->getId() != null){
-            $password = $personne->getPassword();
-            $personne=$personne->setPassword($this->encoder->hashPassword($personne, $password));
+            if(isset($user['password']))
+            {
+                $password = $user['password'];
+                $personne=$personne->setPassword($this->encoder->hashPassword($personne, $password));
+            }
             return $personne;
         }
     }
